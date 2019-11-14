0012d6efd559b4459a2293eae4db9993ae52d64c
lbstc888@163.com <lbstc888@163.com>
2019-11-14 16:05:37 +0800

    middleware

---
+++ code/middleware/app.js
@@ -0,0 +1,32 @@
+const Koa = require('koa');
+const app = new Koa();
+
+//logger
+app.use(async (ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(5);
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+//x-response-time
+app.use(async (ctx, next) => {
+    console.log(2);
+    const start = Date.now();
+    await next();
+    console.log(4);
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+//response
+app.use(async ctx => {
+    console.log(3);
+    ctx.body = 'Hello wolrd';
+});
+
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000');
+});
+
